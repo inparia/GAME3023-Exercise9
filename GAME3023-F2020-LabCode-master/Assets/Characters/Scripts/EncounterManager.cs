97a4caa8c7acead41d6ee1ca5aa681fd800c10d5
inparia <inparia@live.ca>
2020-12-04 23:26:19 -0500

    Exercise 9 Particle System upload

---
+++ GAME3023-F2020-LabCode-master/Assets/Characters/Scripts/EncounterManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EncounterManager : MonoBehaviour
+{
+    public AudioSource mainTheme;
+    public AudioSource battleTheme;
+    public void EnterEncounter()
+    {
+        SceneManager.LoadScene("BattleScene");
+        StartCoroutine(AudioFadeScript.FadeOut(mainTheme, 0.5f));
+        StartCoroutine(AudioFadeScript.FadeIn(battleTheme, 5f));
+    }
+
+    public void ExitEncounter()
+    {
+        // In a full game, your code should remember the player's last area and return there
+        SceneManager.LoadScene("Overworld");
+        StartCoroutine(AudioFadeScript.FadeOut(battleTheme, 0.5f));
+        StartCoroutine(AudioFadeScript.FadeIn(mainTheme, 5f));
+
+    }
+}
